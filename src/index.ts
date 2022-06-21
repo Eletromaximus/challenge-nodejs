4ebd20f1409e4e01fb0d418d57279e85b3ba664f
Max Milliano <maxmillianox@protonmail.com>
2022-06-21 20:36:15 -0300

    feat(adicionando a primeira entidade): docker configurado, container do bd também

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import cors from 'cors'
+import 'express-async-errors'
+import helmet from 'helmet'
 
 const PORT = 3000
 const HOST = '0.0.0.0'
 
 const app = express()
+
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
